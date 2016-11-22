68edbbe7c307df6eb3bb3f714eee082d48606109
Lex Miranda <lexlopes9@hotmail.com>
2016-11-22 06:22:43 -0200

    corrigindo - primeiro commit

---
+++ APM.WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace APM.WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            
+            //hbilita o Cors
+            config.EnableCors();
+
+            //Formata os camelCases ao deserializar o json
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
